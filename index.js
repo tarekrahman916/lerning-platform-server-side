da2d1f5ac861bbc00f51fa42064911788a4a2f4e
Md. Tarek Rahman <tarekrahman237@gmail.com>
2022-10-26 20:18:28 +0600

    created courses data

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-// const express = require("express");
-// const app = express();
-// const cors = require("cors");
-// const port = process.env.PORT || 5000;
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
 
-// app.use(cors());
-// const courses = require("./data/courses.json");
+app.use(cors());
+const courses = require("./data/courses.json");
 
-// app.get("/", (req, res) => {
-//   res.send("server is running");
-// });
-// app.get("/courses", (req, res) => {
-//   res.send(courses);
-// });
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
 
-// app.listen(port, () => {
-//   console.log("Server is running on port:", port);
-// });
+app.listen(port, () => {
+  console.log("Server is running on port:", port);
+});
