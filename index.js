c675407e51524236661c0453d58150ad568b9510
Md. Tarek Rahman <tarekrahman237@gmail.com>
2022-10-26 21:59:57 +0600

    created courses route

--- index.js
+++ index.js
@@ -7,11 +7,17 @@ app.use(cors());
 const courses = require("./data/courses.json");
 
 app.get("/", (req, res) => {
-  res.send("server is running");
+  res.send("Learning website server is running");
 });
 app.get("/courses", (req, res) => {
   res.send(courses);
 });
+
+// app.get("/course/:id", (req, res) => {
+//   const id = req.params.id;
+//   const selectedCourse = courses.find((c) => c.id == id);
+//   res.send(selectedCourse);
+// });
 
 app.listen(port, () => {
   console.log("Server is running on port:", port);
